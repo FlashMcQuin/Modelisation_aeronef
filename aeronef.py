bb1ca0784cf0310670fdd46a618c76859547185b
FlashMcQuin <hugoquin68@gmail.com>
2024-01-07 23:15:04 +0100

    final commit

--- aeronef.py
+++ aeronef.py
@@ -3,6 +3,7 @@ import control as c
 import atm_std as atm
 import sisopy31 as siso
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 class Aeronef():
     def __init__(self, M = 1.21, z=1050):
         self.z0=z*0.3048            #convert feet to meters
@@ -20,25 +21,25 @@ class Aeronef():
         self.l_t= self.l_ref*3/2
         self.g0=9.81
         self.c =0.52
+        self.c_new=self.f*1.1
         self.S=34
         self.rg=2.65                #m
         self.tau = 0.75             #s
-
-        #Centers of gravity
-        self.G = self.c*self.l_t
-        self.F=self.f*self.l_t
-        self.F_delta=self.f_delta*self.l_t
-
-        self.Iyy=self.m*self.rg**2
-
-        self.dx = self.c*self.l_t-self.f*self.l_t
-        self.dy= self.c*self.l_t - self.f_delta*self.l_t
-        # dynamique pressure Pa
+        # dynamic pressure Pa
         self.gamma0 = 0
         self.alpha0 = 0
         self.theta0 = 0
+        self.F=self.f*self.l_t
+        self.F_delta=self.f_delta*self.l_t
+        self.Iyy=self.m*self.rg**2
 
-    def alpha_eq(self, X):
+    def cog(self, c_val):
+        #Centers of gravity
+        self.G = c_val*self.l_t
+        self.dx = c_val*self.l_t-self.f*self.l_t
+        self.dy= c_val*self.l_t - self.f_delta*self.l_t
+
+    def compute_alpha_eq(self, X):
         # X = [V, gamma, aplha, Q, tetha, z]
         alpha0 = X[2]
         Q0 = X[3]
@@ -77,81 +78,113 @@ class Aeronef():
         Returns:
             tuple(A, B, C, D): State space matrixes.
         """
-
         gamma_eq = 0
         Ft=0
         Q = X[3]
-        V_eq = X[0]
+        self.V_eq = X[0]
         # Calculus simplification variables.
-        Cx_eq, Cz_eq, alpha_eq, delta_eq, F_eq = self.alpha_eq(X)
-        print(alpha_eq)
+        Cx_eq, Cz_eq, self.alpha_eq, delta_eq, F_eq = self.compute_alpha_eq(X)
+        print(self.alpha_eq)
         #print(Cx_eq, Cz_eq, alpha_eq, delta_eq, F_eq)
-        QSV = Q*self.S/(self.m*V_eq)
+        QSV = Q*self.S/(self.m*self.V_eq)
         QSI = Q*self.S*self.l_ref/self.Iyy
         Cx_alpha = 2*self.k*Cz_eq*self.Cz_alpha
         Cx_deltam = 2*self.k*Cz_eq*self.Cz_deltam
-        Cm_alpha = self.dx*(Cx_alpha*np.sin(alpha_eq)+self.Cz_alpha*np.cos(alpha_eq))/self.l_ref
-        Cm_deltam = self.dy*(Cx_deltam*np.sin(alpha_eq)+self.Cz_deltam*np.cos(alpha_eq))/self.l_ref
+        Cm_alpha = self.dx*(Cx_alpha*np.sin(self.alpha_eq)+self.Cz_alpha*np.cos(self.alpha_eq))/self.l_ref
+        Cm_deltam = self.dy*(Cx_deltam*np.sin(self.alpha_eq)+self.Cz_deltam*np.cos(self.alpha_eq))/self.l_ref
         
         Xv = 2*QSV*Cx_eq
-        X_alpha = F_eq*np.sin(alpha_eq)/(self.m*V_eq) + QSV*Cx_alpha
-        X_gamma=self.g0*np.cos(gamma_eq)/V_eq
+        X_alpha = F_eq*np.sin(self.alpha_eq)/(self.m*self.V_eq) + QSV*Cx_alpha
+        X_gamma=self.g0*np.cos(gamma_eq)/self.V_eq
         X_deltam = QSV*Cx_deltam
-        Xt=-Ft*np.cos(alpha_eq)/(self.m*V_eq) # =0
+        Xt=-Ft*np.cos(self.alpha_eq)/(self.m*self.V_eq) # =0
 
     
-        m_alpha= QSI*Cm_alpha
-        m_q=QSI*self.l_ref*self.Cm_q/V_eq
-        m_deltam=QSI*Cm_deltam
+        self.m_alpha= QSI*Cm_alpha
+        self.m_q=QSI*self.l_ref*self.Cm_q/self.V_eq
+        self.m_deltam=QSI*Cm_deltam
 
         Zv=2*QSV*Cz_eq
-        Z_alpha=F_eq*np.cos(alpha_eq)/(self.m*V_eq) + QSV*self.Cz_alpha
-        Z_gamma = self.g0*np.sin(gamma_eq)/V_eq   # gamma_eq = 0 -> Z_gamma = 0
-        Z_deltam=QSV*self.Cz_deltam
-        Zt= Ft*np.sin(delta_eq)/self.m*V_eq # =0
+        self.Z_alpha=F_eq*np.cos(self.alpha_eq)/(self.m*self.V_eq) + QSV*self.Cz_alpha
+        Z_gamma = self.g0*np.sin(gamma_eq)/self.V_eq   # gamma_eq = 0 -> Z_gamma = 0
+        self.Z_deltam=QSV*self.Cz_deltam
+        Zt= Ft*np.sin(delta_eq)/self.m*self.V_eq # =0
 
         #print(Xv, X_gamma, X_alpha, Zv, Z_alpha, m_alpha, m_q)
         A = np.array([[-Xv, -X_gamma, -X_alpha, 0, 0, 0],
-                    [   Zv, 0, Z_alpha, 0, 0, 0],
-                    [  -Zv, 0, -Z_alpha, 1, 0, 0],
-                    [   0, 0, m_alpha, m_q, 0, 0],
+                    [   Zv, 0, self.Z_alpha, 0, 0, 0],
+                    [  -Zv, 0, -self.Z_alpha, 1, 0, 0],
+                    [   0, 0, self.m_alpha, self.m_q, 0, 0],
                     [   0, 0, 0, 1, 0, 0],
-                    [   0, V_eq, 0, 0, 0, 0]])
+                    [   0, self.V_eq, 0, 0, 0, 0]])
         
-        B = np.array([0, Z_deltam, -Z_deltam, m_deltam, 0, 0])
-        B=B.reshape(-1,1)
+        B = np.array([[0, self.Z_deltam, -self.Z_deltam, self.m_deltam, 0, 0]]).T
         C = np.zeros((1,6))
         C[0,2]=1
         D = np.zeros((1,1))
 
         return A, B, C, D
     
+    def short_period(self, A, B):
+        Asp = A[2:4 , 2:4]
+        Bsp = B[2:4 , 0:1]
+        Csp_alpha = np.matrix ( [[1 , 0]] )
+        Csp_q = np.matrix ( [[0 , 1]] )
+        Dsp = np.matrix ( [[0]])
+
+        sp_alpha_ss = c.ss(Asp,Bsp,Csp_alpha,Dsp)
+        c.matlab.damp(sp_alpha_ss)
+        print( "\n\nTransfer function alpha / delta_m =" )
+        sp_alpha_tf = c.matlab.ss2tf(sp_alpha_ss)
+        print(sp_alpha_tf)
+        print ( "Static gain of alpha / delta_m = %f "%(c.dcgain(sp_alpha_ss)))
+
+        sp_q_ss = c.ss(Asp,Bsp,Csp_q,Dsp)
+        print( "\n\nTransfer function q / delta_m =" )
+        sp_q_tf = c.matlab.ss2tf(sp_q_ss)
+        print(sp_q_tf)
+        print ( "Static gain of q / delta_m = %f "%(c.dcgain(sp_q_ss)))
+        return sp_alpha_ss, sp_alpha_tf, sp_q_ss, sp_q_tf
+    
+    def phugoid_mode(self, A, B):
+        Ap=A[ 0 : 2 , 0 : 2 ]
+        Bp=B[ 0 : 2 , 0 : 1 ]
+        Cpv = np.matrix ( [[1 , 0]] )
+        Cpg = np.matrix ( [[0 , 1]] )
+        Dp = np.matrix ( [[0]])
+        ph_v_ss =  c.ss(Ap ,Bp ,Cpv ,Dp)
+        c.matlab.damp(ph_v_ss)
+        print( "\n\nTransfer function V / delta_m =" )
+        ph_v_tf = c.tf ( ph_v_ss )
+        print( ph_v_tf )
+        print ( "Static gain of V / delta_m = %f "%(c.dcgain(ph_v_tf)))
+
+        ph_gammma_ss = c.ss(Ap ,Bp ,Cpg ,Dp)
+        print( "\n\nTransfer function gamma / delta_m =" )
+        ph_gammma_tf = c.matlab.ss2tf(ph_gammma_ss)
+        print(ph_gammma_tf)
+        print ( "Static gain of q / delta_m = %f "%(c.dcgain(ph_gammma_ss)))
+        return ph_v_ss, ph_v_tf, ph_gammma_ss, ph_gammma_tf
+    
     def open_loop(self, A, B, C, D, mode, title):
         ss = c.ss(A, B, C, D)
         ri,a,b,xi,w,st = siso.damp(ss)
         print(f"\n----------------- {mode} Mode :    --------------------------------------\n ")
-        for i in st : 
+        for i in st :
             print(i)
         tf = siso.ss2tf(ss)
         print(f"Transfer function : \n ", tf)
         Y, t=c.matlab.step(tf, 10)
         plt.plot(t,Y, label= title)
-        plt.title("(Open Loop) Step Response"+title)
+        plt.title("(Open Loop) Step Response "+title)
         plt.xlabel("Time (s)")
         plt.ylabel("(rad)")
 
-    def find_k(self,A,B,C,D):
-        ss = c.ss(A, B, C, D)
-        k = siso.sisotool(ss)
-        return k
     
-    def correction_open_loop(self, A, B, C, D, title, k):
+    def correction_open_loop(self, A, B, C, D, k):
         # k was found with the use of sisotool on the line above
         print("k = ", k)
-        print(B)
-        print("@")
-        print(C)
-        Ak = A-k*B*C
+        Ak = A-k*np.dot(B,C)
         Bk = k*B
         Dk = k*D
         closed_loop_ss = c.ss(Ak, Bk, C, Dk)
@@ -160,7 +193,34 @@ class Aeronef():
         ri,a,b,xi,w,st = siso.damp(closed_loop_ss)
         for i in st : 
             print(i)
+        return Ak, Bk, closed_loop_ss, closed_loop_tf
 
-        return Ak, Bk
-
+    def max_incidence(self, A, B, C, D, gamma, alpha_eq,alpha_0):
+        delta_nz = 3.1*9.81
+        alpha_max = alpha_eq + (alpha_eq - alpha_0)*delta_nz
+        syst_ss_alpha = c.ss(A,B*gamma,C,D)
+        syst_tf_alpha = c.tf(c.minreal(syst_ss_alpha))
+        
+        alpha,t_alpha = c.matlab.step(syst_tf_alpha)
+        response = max(alpha)-alpha_max
+        return response
+    
+    def saturation(self, A, B, C, D): 
+        epsilon = pow(10,-5)
+        err = 1
+        gamma = 0.1
+        i = 0
+        while err > epsilon and i<100 :
+            gamma_old = gamma
+            response_1 = self.max_incidence(A, B, C, D, gamma, self.alpha_eq,self.alpha0)
+            response_2 = self.max_incidence(A, B, C, D, gamma+0.01, self.alpha_eq, self.alpha0)
+            print(f"response_1 = {response_1} / response_2 = {response_2} ")
+            df = (response_2 - response_1)/ 0.01
+            print(f"df = {df}")
+            gamma = gamma - (response_1/df)
+            print(f"gamma = {gamma}")
+            err = abs(gamma-gamma_old)
+            print(f"Error = {err}")
+            i+=1    
+        print(f"Gamma max : {(gamma*180/np.pi)}")
 
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+"""
+Author : Hugo Quin
+
+"""
 import numpy as np
 import sisopy31 as siso
 import atm_std as atm
 from aeronef import *
 import control
+
 aero = Aeronef()
+aero.cog(aero.c) #define the value of c
 M = 1.21
 
 z0=1050*0.3048   #ft
@@ -24,11 +30,7 @@ X = np.array([Veq,
               z0]).T
 
 A,B,C,D = aero.state_space(X)
-
-state_space = control.ss(A,B,C,D)
-print("control minreal : ",control.minreal(state_space))
-print("State space : ",state_space)
-
+state_space = c.ss(A,B,C,D)
 ri,a,b,xi,w,st = siso.damp(state_space)
 
 for i in st : 
@@ -50,88 +52,208 @@ with a low damping ratio at 0.215. -> alpha & q
 The system possesses a slow mode at approx 0.03 rad/s
 with a damping ratio at 1. -> gamma & V (phugoid)
 """
-#fig, axs = plt.subplots(2, 2)
-
 # x = [gamma, alpha, q, theta, z]
+"""
+--------------------Step responses in short-period mode : alpha and q ------------------------
+"""
+sys_a, tf_a, sys_q, tf_q = aero.short_period(A, B)
+
+Ya , Ta = c.matlab.step(sys_a , 10 )
+Yq , Tq = c.matlab.step(sys_q , 10 )
+plt.plot(Ta ,Ya , 'b' , Tq ,Yq , 'r', lw=2)
+step_info1 = c.step_info(Ya,Ta)
+Tsa = step_info1['SettlingTime']
+Tra = step_info1['RiseTime']
+Osa = step_info1['Overshoot']
+yya = interp1d(Ta,Ya)
+plt.plot(Tsa, yya(Tsa),'bs')
+plt.text(Tsa, yya(Tsa)+0.2, round(Tsa,2))
+step_info2 = c.step_info(Yq,Tq)
+Tsq = step_info2['SettlingTime']
+Trq = step_info2['RiseTime']
+Osq = step_info2['Overshoot']
+yyq = interp1d(Tq,Yq)
+plt.plot(Tsq, yyq(Tsq),'rs')
+plt.text(Tsq, yyq(Tsq)-0.3, Tsq)
+print('alpha Setlling time 5%% = %f s'%Tsa)
+print('q Setlling time 5%%     = %f s'%Tsq)
+plt.title(r' Step response $\alpha/\delta_m$ and $q/\delta_m$')
+plt.legend((r'$\alpha/\delta_m$ ', r'$q/\delta_m$'))
+plt.xlabel ( 'Time (s)' )
+plt.ylabel ( r'$\alpha$ (rad) & $q$ ( rad/s )' )
+plt.show()
+
+"""
+------------- Step responses for phugoid mode : v and gamma -------------------------------
+"""
+
+ph_v_ss, ph_v_tf, ph_gammma_ss, ph_gammma_tf= aero.phugoid_mode(A,B)
+Yv , Tv = c.matlab.step(ph_v_ss, 700)
+Yg , Tg = c.matlab.step(ph_gammma_ss, 700 )
+plt.plot(Tv ,Yv , 'b' , Tg ,Yg , 'r', lw=2)
+step_info1 = c.step_info(Yv,Tv)
+Tsv = step_info1['SettlingTime']
+Trv = step_info1['RiseTime']
+Osv = step_info1['Overshoot']
+yyv = interp1d(Tv,Yv)
+plt.plot(Tsv, yyv(Tsv),'bs')
+plt.text(Tsv, yyv(Tsv)-0.2, Tsv)
+step_info2 = c.step_info(Yg,Tg)
+Tsg = step_info2['SettlingTime']
+Trg = step_info2['RiseTime']
+Osg = step_info2['Overshoot']
+yyg=interp1d(Tg,Yg)
+plt.plot(Tsg, yyg(Tsg),'rs')
+plt.text(Tsg, yyg(Tsg)-0.2, Tsg)
+print('alpha Setlling time 5%% = %f s'%Tsv)
+print('q Setlling time 5%%     = %f s'%Tsg)
+plt.title(r' Step response $V/\delta_m$ and $\gamma/\delta_m$')
+plt.legend((r'$V/\delta_m$ ', r'$\gamma/\delta_m$'))
+plt.xlabel ( 'Time (s)' )
+plt.ylabel ( r'$V$ (rad) & $\gamma$ ( rad/s )' )
+plt.show()
 
 #Perfect Auto-throttle
 A_new = A[1:, 1:] # size 5 without V
 B_new = B[1:] # size 5 without V
-print("B_new size : ", B_new.size)
-C_gamma= np.array([1,0,0,0,0])
-C_alpha=np.array([0,1,0,0,0])
-C_q= np.array([0,0,1,0,0])
-C_z= np.array([0,0,0,0,1])
 
+C_gamma= np.array([[1,0,0,0,0]])
+C_alpha=np.array([[0,1,0,0,0]])
+C_q= np.array([[0,0,1,0,0]])
+C_z= np.array([[0,0,0,0,1]])
 
-aero.open_loop(A_new, B_new, C_alpha, D, mode = "", title = " alpha")
+aero.open_loop(A_new, B_new, C_q, D, mode = "", title = " q")
+plt.show()
 
-
-#aero.open_loop(A_new, B_new, C_q, D, mode = "", title= " C_q")
-
-
+"""
+-------------------- q feedback loop : --------------------------------
+"""
 
 #k=aero.find_k(A_new, B_new, C_q, D)
-k=-0.08528140111595887
-Aq, Bq = aero.correction_open_loop(A_new, B_new, C_q, D, title = "Calpha", k=k)
+Kr=-0.08528140111595887
 
-sys_cq = c.ss(A_new, B_new, C_q, D) # Aq et Bq
-sys_calpha = c.ss(A_new, B_new, C_alpha, D)
-
-block1 = c.feedback(k, sys_cq)
-block2 = c.series(block1,sys_calpha)
-Y, t = c.matlab.step(block2, 10) # 10 seconds
+Aq, Bq, sys_q, tf_q= aero.correction_open_loop(A_new, B_new, C_q, D, k=Kr)
+print("Aq : ", Aq, "Bq : ", Bq)
+Y, t = c.matlab.step(sys_q, 10) # 10 seconds
+plt.plot(t, Y)
+plt.title("Step response of the closed loop (q feedback loop)")
+plt.xlabel("Time (s)")
+plt.ylabel("Pitch Rotation Speed (rad/s)")
+plt.show()
+sys_calpha = c.ss(Aq, Bq, C_alpha, D)
+"""
+block1 = c.feedback(Kr, sys_q)
+block_q = c.series(block1,sys_calpha)
+Y, t = c.matlab.step(block_q, 10) # 10 seconds
 plt.plot(t, Y, label="alpha with kq")
 plt.title("Closed loop alpha")
 plt.legend()
+"""
 
-############### Washout filter part #######################
-to = 0.75
+""" 
+--------Washout filter : ------------
+"""
+# t = 2/w_sp
+to = 2/0.2145
+#to = 0.75
 washout= c.tf([to, 0], [to,1])
-block1 = c.series(washout,c.ss2tf(sys_cq))
-block2 = c.feedback(k, block1)
-block3 = c.series(1/k, block2, sys_calpha)
-Y, t=c.matlab.step(block3, 10) #10 seconds
-plt.plot(t, Y, label="alpha with kq and washout filter")
-plt.title("Washout filter")
-plt.xlabel("t(s)")
-plt.ylabel("alpha (rad)")
+sys_FTBO = tf_a
+sys_FTBF = c.series(1/Kr,c.feedback(Kr,tf_q),tf_a) 
+sys_FTBF_filter = c.series(1/Kr , c.feedback(Kr,c.series(tf_q, washout)),tf_a) 
+
+T = 10
+step_1 , t_1 = c.matlab.step(sys_FTBO,T = T)
+step_2 , t_2 = c.matlab.step(sys_FTBF,T = T)
+step_3 , t_3 = c.matlab.step(sys_FTBF_filter,T = T)
+
+plt.plot(t_1,step_1,label = "Open Loop")
+plt.plot(t_2,step_2,label = "Closed Loop")
+plt.plot(t_3,step_3,label = "Closed Loop and Washout Filter")
+
 plt.legend()
+plt.title("Step response")
+plt.xlabel("Time (in s)")
+plt.ylabel(r'$\alpha$')
 plt.show()
-#########################################################
 
+""" 
+--------------------- Gamma feedback loop : ---------------------------
+"""
 
+aero.open_loop(Aq, Bq, C_gamma, D, mode = "", title = "of the flight path angle")
+plt.show()
 #kgamma=aero.find_k(Aq, Bq, C_gamma, D)
-kgamma =  17.97283
+kgamma =  17.97283 # second tuning
 
-Agamma, Bgamma = aero.correction_open_loop(Aq, Bq, C_gamma, D, title = "Cgamma", k=kgamma)
-sys_cgamma = c.ss(Aq, Bq, C_gamma, D)
+Agamma, Bgamma, sys_gamma, tf_gamma = aero.correction_open_loop(Aq, Bq, C_gamma, D, k=kgamma)
+print("Agamma : ", Agamma, "Bgamma : ", Bgamma)
 
-Y, t = c.matlab.step(sys_cgamma, 10) # 10 seconds
-plt.plot(t, Y, label="z with kgamma")
-plt.title("Closed loop ")
-plt.legend()
+Y, t = c.matlab.step(tf_gamma, 10) # 10 seconds
+plt.plot(t, Y)
+plt.title("Step response of the flight path angle hold mode corrected with kgamma")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
 plt.show()
 
-sys_gamma = c.ss(Aq, Bq, C_gamma, D)
-block4 = c.feedback(kgamma, sys_gamma)
-Y, t = c.matlab.step(block4, 10) # 10 seconds
-plt.plot(t, Y, label="z with kgamma")
-plt.title("Closed loop corrected with kgamma ")
-plt.legend()
+""" 
+------------------------------ z feedback loop : ------------------------------------
+"""
+aero.open_loop(Aq, Bq, C_gamma, D, mode = "", title = " z")
 plt.show()
-
-
 #kz = aero.find_k(Agamma, Bgamma, C_z, D) #trouvé
-kz= 0.00392
+kz= 0.00411
+Az, Bz, sys_z, tf_z = aero.correction_open_loop(Agamma, Bgamma, C_z, D, k=kz)
+print("Az : ", Az, "Bz : ", Bz)
 
-Az, Bz = aero.correction_open_loop(Agamma, Bgamma, C_z, D, title = "Cgamma", k=kz)
-
-sys_z = c.ss(Az, Bz, C_gamma, D)
-block5 = c.feedback(kz, sys_z)
-Y, t = c.matlab.step(block5, 10) # 10 seconds
+Y, t = c.matlab.step(sys_z, 10) # 10 seconds
 plt.plot(t, Y)
 plt.title("Closed loop corrected with kz ")
+plt.show()
+
+""" 
+----------------------------- Staturation on gamma control loop :  --------------------------------
+"""
+A_sat = Agamma 
+B_sat = Bgamma 
+C_sat = np.array([[0, 1, 0, 0, 0]])
+D_sat = D
+syst_sat =  control.ss(A_sat,B_sat,C_sat,D_sat)
+syst_tf_sat = control.tf(syst_sat)
+
+aero.saturation(A_sat,B_sat,C_sat,D_sat)
+
+""" 
+------------------------ New C.O.G -------------------------
+"""
+aero.cog(aero.c_new) #change value of c to c_new
+A_new_cog, B_new_cog, C_new_cog, D_new_cog = aero.state_space(X)
+A_new_cog = A[1:, 1:] # size 5 without V
+B_new_cog = B[1:] # size 5 without V
+C_new_cog = np.array([[0, 1, 0, 0, 0]])
+
+sys_new_cog =  control.ss(A_new_cog, B_new_cog, C_new_cog, D_new_cog)
+sys_tf_new_cog = control.tf(sys_new_cog)
+print("A = ",A_new_cog,"B = ", B_new_cog)
+print(sys_tf_new_cog)
+control.matlab.damp(sys_new_cog)
+
+plt.figure(10)
+q_new , t_new = control.matlab.step(sys_tf_new_cog)
+plt.plot(t_new,q_new)
+plt.title("Step response of the new system model")
+plt.xlabel("Time (s)")
+plt.ylabel(r'$\alpha$')
+plt.show()
+
+siso.sisotool(sys_tf_new_cog)
+kalpha= 0.19729
+
+Aalpha, Balpha, sys_alpha, tf_alpha = aero.correction_open_loop(A_new_cog, B_new_cog, C_new_cog, D, k=kalpha)
+y,t=c.matlab.step(sys_alpha, 10)
+plt.plot(t,y)
+plt.title(r"Step response with control feedback on $\alpha$")
+plt.xlabel("Time (s)")
+plt.ylabel(r'$\alpha$')
 plt.show()
 
 
